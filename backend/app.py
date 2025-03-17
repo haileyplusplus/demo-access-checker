1d1d5662f2dabf3b0c5d5ba32fdc4655cf813f01
hailey <188331+haileyplusplus@users.noreply.github.com>
2025-03-17 17:18:24 -0500

    Remove complexity and add very simple cookie state for testing.

--- backend/app.py
+++ backend/app.py
@@ -1,52 +1,11 @@
-"""
-Original code from https://raw.githubusercontent.com/fastapi/fastapi/refs/heads/master/docs_src/security/tutorial005_an_py310.py
-Retrieved March 17, 2025
-
-The MIT License (MIT)
-
-Copyright (c) 2018 Sebastián Ramírez
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
-
 import json
 from pathlib import Path
-
-from datetime import datetime, timedelta, timezone
 from typing import Annotated
 
-import jwt
-from fastapi import Depends, FastAPI, HTTPException, Security, status
-from fastapi.security import (
-    OAuth2PasswordBearer,
-    OAuth2PasswordRequestForm,
-    SecurityScopes,
-)
-from jwt.exceptions import InvalidTokenError
-from passlib.context import CryptContext
-from pydantic import BaseModel, ValidationError
+from fastapi import Cookie, FastAPI, HTTPException, Response
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+app = FastAPI()
 
 
 LOCAL_DIRECTORY = Path(__file__).parent
@@ -58,166 +17,54 @@ def load_user_config():
     with user_config.open() as fh:
         users = json.load(fh)['users']
         for user in users:
-            users_db[user['id']] = {
-                "username": user['id'],
-                "full_name": user['name'],
-                "email": user['id'],
-                "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-                "disabled": False
-            }
+            users_db[user['id']] = user
     return users_db
 
 
-def load_scope_config():
-    scope_config = LOCAL_DIRECTORY.parent / 'config' / 'accessprofiles.json'
-    scopes = {}
-    with scope_config.open() as fh:
+def load_profile_config():
+    profile_config = LOCAL_DIRECTORY.parent / 'config' / 'accessprofiles.json'
+    configured = {}
+    with profile_config.open() as fh:
         profiles = json.load(fh)['profiles']
         for profile in profiles:
             id_ = profile['name']
-            scopes[id_] = profile['description']
-    return scopes
+            configured[id_] = profile
+    return configured
 
 
-fake_users_db = load_user_config()
+users = load_user_config()
+profiles = load_profile_config()
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
+@app.get('/cookieset')
+def cookie_set(arg: str, response: Response):
+    #print(response.__dict__)
+    response.set_cookie(key="user_scope", value=arg)
+    return {'cookie':
+            'set2'}
 
 
-class TokenData(BaseModel):
-    username: str | None = None
-    scopes: list[str] = []
+@app.get('/cookietest')
+def cookie_test(user_scope: Annotated[str | None, Cookie()] = None):
+    return {'cookie': user_scope}
 
 
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
+@app.get('/set-user')
+def set_user(username: str, response: Response):
+    if username not in users:
+        raise HTTPException(status_code=404, detail=f"User {username} not found.")
+    response.set_cookie(key="user", value=username)
+    return {'status': 'ok'}
 
 
-class UserInDB(User):
-    hashed_password: str
+@app.get('/get-user')
+def get_user(user: Annotated[str | None, Cookie()] = None, profile: Annotated[str | None, Cookie()] = None):
+    return {'user': user, 'profile': profile}
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl="token",
-    scopes=load_scope_config(),
-)
-
-app = FastAPI()
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(
-    security_scopes: SecurityScopes, token: Annotated[str, Depends(oauth2_scheme)]
-):
-    if security_scopes.scopes:
-        authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
-    else:
-        authenticate_value = "Bearer"
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": authenticate_value},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_scopes = payload.get("scopes", [])
-        token_data = TokenData(scopes=token_scopes, username=username)
-    except (InvalidTokenError, ValidationError):
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    for scope in security_scopes.scopes:
-        if scope not in token_data.scopes:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Not enough permissions",
-                headers={"WWW-Authenticate": authenticate_value},
-            )
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Security(get_current_user, scopes=["me"])],
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-@app.post("/token")
-async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-) -> Token:
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username, "scopes": form_data.scopes},
-        expires_delta=access_token_expires,
-    )
-    return Token(access_token=access_token, token_type="bearer")
-
-
-@app.get("/users/me/", response_model=User)
-async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)],
-):
-    return current_user
-
-
-@app.get("/users/me/items/")
-async def read_own_items(
-    current_user: Annotated[User, Security(get_current_active_user, scopes=["items"])],
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]
-
-
-@app.get("/status/")
-async def read_system_status(current_user: Annotated[User, Depends(get_current_user)]):
-    return {"status": "ok"}
+@app.get('/set-profile')
+def set_profile(profile_name: str, response: Response):
+    if profile_name not in profiles:
+        raise HTTPException(status_code=404, detail=f"Profile {profile_name} not found.")
+    response.set_cookie(key="profile", value=profile_name)
+    return {'status': 'ok'}
